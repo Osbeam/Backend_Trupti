f4e8d74d684e3b6a158f5f1daa9ee411d4beea26
Osbeam <osbeamit1@gmail.com>
2024-10-09 17:34:31 +0530

    index change

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const socketIO = require('socket.io');
 const http = require('http');
 
 
+const { loadMessages, sendMessage, fetchGroupChat, createGroupChat, AddNewMemberInGroupChat, getAllUser,getUserByDepartment } = require("./src/controller/chatController.js"); 
+
 const app = express();
 app.use('/uploads', express.static('uploads'))
 const PORT = process.env.PORT || 8000;
@@ -21,6 +23,7 @@ app.use(express.json());
 
 // connecting with database
 const mongoose = require("mongoose");
+
 mongoose.connect(process.env.DB_STRING
 ).then(()=>{
     console.warn("db connection done")
@@ -38,6 +41,54 @@ const io = socketIO(server1, {
 const onlineUsers = new Map();
 
 io.on("connection", (socket) => {
+
+// chat module functions
+
+// load Users
+socket.on("loadUser", () => {
+  getAllUser(socket); 
+});
+socket.on("loadUserByDepartment", (data) => {
+  getUserByDepartment(data,socket); 
+});
+
+
+// Handle loading messages
+socket.on("loadMessages", (data) => {
+  
+  loadMessages(data, socket); 
+});
+
+// fetch chat rooms
+socket.on("loadGroupChat", (data) => {
+  
+  fetchGroupChat(data, socket); 
+});
+
+// Handle sending messages
+socket.on("sendMessage", (messageData) => {
+  sendMessage(messageData, io); 
+});
+
+// Handle group chats 
+socket.on("createGroupChat", (groupDetails) => {
+  createGroupChat(groupDetails, io); 
+});
+
+socket.on("newMember", (groupAndMemberDetails) => {
+  AddNewMemberInGroupChat(groupAndMemberDetails, io); 
+});
+
+
+
+
+
+
+
+
+
+
+
   socket.on("connectSocket", (userId) => {
     onlineUsers.set(userId, socket.id);
     io.emit("onlineUsers", Array.from(onlineUsers.keys()));
@@ -67,6 +118,6 @@ app.all("*", (req, res) => res.status(404).json({ error: "404 Not Found" }));
 
 
 
-const server = app.listen(PORT, () =>
+const server = server1.listen(PORT, () =>
   console.log(`Server running on ${process.env.BACKEND_URL}`)
 );
